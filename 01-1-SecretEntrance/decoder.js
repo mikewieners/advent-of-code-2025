c044ca563a8f2ae84fe8d8156880b1e092058e97
Mike Wieners <mwieners@triahealth.com>
2025-12-01 10:46:46 -0600

    initial commit

---
+++ 01-1-SecretEntrance/decoder.js
@@ -0,0 +1,10 @@
+
+// Get the input data from https://adventofcode.com/2025/day/1/input
+
+// Process the data
+// For each entry, turn the dial, set the resting point, increment count if the dial lands on zero
+var msg = 'The entry code is 99';
+console.log(msg);
+
+
+// After processing all of the input data, display the zero count
