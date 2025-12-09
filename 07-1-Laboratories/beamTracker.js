54d0d678632b1eb28b5652a4a65d0f84fc0c63f0
Mike Wieners <mike.wieners@gmail.com>
2025-12-08 22:53:35 -0600

    finish 7-1

--- 07-1-Laboratories/beamTracker.js
+++ 07-1-Laboratories/beamTracker.js
@@ -1,17 +1,65 @@
 
 import { manifold } from './testData.js';
+
+let manifoldDiagramResult = [manifold[0]];
+let rowLength = manifold[0].length;
 let splitCount = 0;
 
-let beamLocations = [];
-beamLocations = findCharacterIndices(manifold[0], 'S');
-for (let i = 1; i < manifold.length; i++) {
+let beamStartIndex = manifold[0].indexOf('S');
+let lastRowBeamIndices = [beamStartIndex];
 
-}
+function findTheSplits() {
+    for (let i = 1; i < manifold.length; i++) {
+        let thisRow = manifold[i];
+        let thisRowResult = [];
+        let newBeamLocations = [];
+        if (i % 2 != 0) {
+            for (let r = 0; r < rowLength; r++) {
+                if (lastRowBeamIndices.includes(r)) {
+                    thisRowResult.push('|');
+                    newBeamLocations.push(r);
+                    continue;
+                }
+                thisRowResult.push('.');
+            }
+            lastRowBeamIndices = []
+            newBeamLocations.forEach(i => {
+                lastRowBeamIndices.push(i);
+            });
+            manifoldDiagramResult.push(thisRowResult);
+            continue;
+        }
+        
+        for (let r = 0; r < rowLength; r++) {
+            if (lastRowBeamIndices.includes(r)) {
+                if (thisRow[r] === '^') {
+                    newBeamLocations.push(r-1);
+                    newBeamLocations.push(r+1);
+                    thisRowResult.push('^');
+                    splitCount++
+                    continue;
+                }
 
-function findCharacterIndices(arr, char) {
-    let charIndices = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === char) { charIndices.push(i) }
+                newBeamLocations.push(r)
+                thisRowResult.push('|');
+                continue;
+            }
+            thisRowResult.push('.');
+        }
+        lastRowBeamIndices = []
+        newBeamLocations.forEach(i => {
+            lastRowBeamIndices.push(i);
+        });
+        manifoldDiagramResult.push(thisRowResult);
     }
-    return charIndices;
+    manifoldDiagramResult.forEach(l => {
+        let logString = '';
+        l.forEach(c => {
+            logString += c;
+        });
+        console.log(logString);
+    });
+    console.log(`Total splits: ${splitCount}`)
 }
+
+findTheSplits();
